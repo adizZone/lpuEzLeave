25da967cc6212d26f6754dad934acab16ea640ce
Aditya <aditya@Kali.net>
2024-02-08 11:46:18 +0530

    probably fixed driver issue this time

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
-import time
+# import time
 
 
 def webScrapper(username, password, leaveType, visitPlace, stayAddress, rMob, sdt, edt, reason):
