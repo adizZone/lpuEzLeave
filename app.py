a355daaeb080d6b630a14f221915e0f2f4e415fd
Aditya <121631690+adizZone@users.noreply.github.com>
2024-02-08 10:42:48 +0530

    fixing webdriver permissions issue attempt-2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
